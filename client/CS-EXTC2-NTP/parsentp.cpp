2e95d29e297e111934cd73cf4525d64235b012dd
Ryan Kleffman <91687869+ryanq47@users.noreply.github.com>
2025-09-17 11:51:23 -0700

    move data to this->packetStruct, and make a calss var

--- client/CS-EXTC2-NTP/parsentp.cpp
+++ client/CS-EXTC2-NTP/parsentp.cpp
@@ -34,27 +34,24 @@ NTPPacketParser::NTPPacketParser(std::vector<uint8_t> ntpPacket) {
         return;
     }
 
-    //move this into a class var so it can be accessed everywhere
-    PacketData data;
-
     // Manually copy bytes from packet into struct fields, rather than casting, for manual control/just incase the packet is malformed, etc.
-    std::memcpy(&data.li_vn_mode, ntpPacket.data(), sizeof(data.li_vn_mode));
-    std::memcpy(&data.stratum, ntpPacket.data() + 1, sizeof(data.stratum));
-    std::memcpy(&data.poll, ntpPacket.data() + 2, sizeof(data.poll));
-    std::memcpy(&data.precision, ntpPacket.data() + 3, sizeof(data.precision));
-    std::memcpy(&data.root_delay, ntpPacket.data() + 4, sizeof(data.root_delay));
-    std::memcpy(&data.root_dispersion, ntpPacket.data() + 8, sizeof(data.root_dispersion));
-    std::memcpy(&data.reference_id, ntpPacket.data() + 12, sizeof(data.reference_id));
-    std::memcpy(&data.reference_ts, ntpPacket.data() + 16, sizeof(data.reference_ts));
-    std::memcpy(&data.originate_ts, ntpPacket.data() + 24, sizeof(data.originate_ts));
-    std::memcpy(&data.receive_ts, ntpPacket.data() + 32, sizeof(data.receive_ts));
-    std::memcpy(&data.transmit_ts, ntpPacket.data() + 40, sizeof(data.transmit_ts));
+    std::memcpy(&this->packetStruct.li_vn_mode, ntpPacket.data(), sizeof(this->packetStruct.li_vn_mode));
+    std::memcpy(&this->packetStruct.stratum, ntpPacket.data() + 1, sizeof(this->packetStruct.stratum));
+    std::memcpy(&this->packetStruct.poll, ntpPacket.data() + 2, sizeof(this->packetStruct.poll));
+    std::memcpy(&this->packetStruct.precision, ntpPacket.data() + 3, sizeof(this->packetStruct.precision));
+    std::memcpy(&this->packetStruct.root_delay, ntpPacket.data() + 4, sizeof(this->packetStruct.root_delay));
+    std::memcpy(&this->packetStruct.root_dispersion, ntpPacket.data() + 8, sizeof(this->packetStruct.root_dispersion));
+    std::memcpy(&this->packetStruct.reference_id, ntpPacket.data() + 12, sizeof(this->packetStruct.reference_id));
+    std::memcpy(&this->packetStruct.reference_ts, ntpPacket.data() + 16, sizeof(this->packetStruct.reference_ts));
+    std::memcpy(&this->packetStruct.originate_ts, ntpPacket.data() + 24, sizeof(this->packetStruct.originate_ts));
+    std::memcpy(&this->packetStruct.receive_ts, ntpPacket.data() + 32, sizeof(this->packetStruct.receive_ts));
+    std::memcpy(&this->packetStruct.transmit_ts, ntpPacket.data() + 40, sizeof(this->packetStruct.transmit_ts));
 
     // Now you can access the fields directly
-    std::cout << "li_vn_mode: " << static_cast<int>(data.li_vn_mode) << std::endl;
-    std::cout << "stratum: " << static_cast<int>(data.stratum) << std::endl;
-    std::cout << "root_delay: " << data.root_delay << std::endl;
-    std::cout << "reference_ts: " << data.reference_ts << std::endl;
+    std::cout << "li_vn_mode: " << static_cast<int>(this->packetStruct.li_vn_mode) << std::endl;
+    std::cout << "stratum: " << static_cast<int>(this->packetStruct.stratum) << std::endl;
+    std::cout << "root_delay: " << this->packetStruct.root_delay << std::endl;
+    std::cout << "reference_ts: " << this->packetStruct.reference_ts << std::endl;
 
 
     //also - don't bother adding the ext field to the struct,just manually extract the extension field based on length, etc. rather than adding complications with struct stuff
--- client/CS-EXTC2-NTP/parsentp.hpp
+++ client/CS-EXTC2-NTP/parsentp.hpp
@@ -7,4 +7,9 @@ public:
     static constexpr size_t BASE_PACKET_SIZE = sizeof(PacketData);
 
     NTPPacketParser(std::vector<uint8_t> ntpPacket);
+
+private:
+    //move this into a class var so it can be accessed everywhere
+    PacketData packetStruct;
 };
+
